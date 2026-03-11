8dec153e7624c1d7c1e21985766c7437142a9235
Brendan Fullerton <bpf2002@comcast.net>
2026-03-11 13:35:59 -0400

    Filled out base project structure with main.py, config.py, and .gitignore

---
+++ config.py
@@ -0,0 +1,22 @@
+
+"""
+
+This file contains variables and parameters for the project,
+such as file paths, model parameters, and other configuration settings.
+
+"""
+# The language model to use for translation
+LANGUAGE_MODEL = "facebook/nllb-200-distilled-600M" # NLLB
+# LANGUAGE_MODEL = "csebuetnlp/mT5_multilingual_XLSum" # mT5
+
+# Source and target languages for translation
+SOURCE_LANGUAGE = "en"
+# SOURCE_LANGUAGE = "eng_Latn" # FLORES-200 BCP-47 code for English if needed
+TARGET_LANGUAGE = "ltz_Latn" # Luxembourgish
+# TARGET_LANGUAGE = "lim_Latn" # Limburgish
+# TARGET_LANGUAGE = "cat_Latn" # Catalan
+# TARGET_LANGUAGE = "oci_Latn" # Occitan
+# TARGET_LANGUAGE = "isl_Latn" # Icelandic
+# TARGET_LANGUAGE = "fao_Latn" # Faroese
+# TARGET_LANGUAGE = "ydd_Hebr" # Eastern Yiddish
+# Add more languages as needed
---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+def main():
+    print("This is the main function of the Transl-AI-tor project.")
+
+if __name__ == "__main__":
+    main()
